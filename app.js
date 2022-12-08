b6a3d6c815fba19d0c625a55e423e8fc87914abf
Mitesh073 <113135872+Mitesh073@users.noreply.github.com>
2022-12-08 17:30:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-/* eslint-disable no-unused-vars */
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-app.get("/", (request, response) => {
-  response.send("Go to localhost:3000/todos");
+app.get("/", function (request, response) {
+  response.send("Hello World");
 });
 
-app.get("/todos", (request, response) => {
-  response.send("Hello Developers!!");
-  //   console.log("Todo list");
-});
-
-app.post("/todos", async (request, response) => {
-  console.log("Creating a Todo", request.body);
+app.get("/todos", async function (_request, response) {
+  console.log("Processing list of all Todos ...");
   try {
-    const todo = await Todo.addTodo({
-      title: request.body.title,
-      dueDate: request.body.dueDate,
-      completed: false,
-    });
+    const todos = await Todo.getAllTodos();
+    return response.json(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
     return response.json(todo);
   } catch (error) {
     console.log(error);
@@ -29,12 +29,20 @@ app.post("/todos", async (request, response) => {
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async (request, response) => {
-  console.log("We have to update a new todo with ID:", request.params.id);
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedTodo = await todo.markAsCompleted();
-    console.log(updatedTodo);
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
